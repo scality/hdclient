d90d407620f484cec13945f09f2b38b22c9135f2
Maxime Lubin <maxime.lubin@scality.com>
2018-07-13 15:57:11 +0200

    RING-28615 PUT functional tests
    Added multiple endpoints suite (replication & erasure coding),
    validation of framgent error persistence and behavior
    when failing to do so.
    
    Signed-off-by: Maxime Lubin <maxime.lubin@scality.com>

--- tests/functional/test_put.js
+++ tests/functional/test_put.js
@@ -17,6 +17,8 @@ mocha.describe('PUT', function () {
     // Verify all mocks have been used - no garbage
     mocha.afterEach(function () { assert.ok(nock.isDone); });
 
+    const deleteTopic = hdclient.httpUtils.topics.delete;
+
     mocha.describe('Single hyperdrive', function () {
         mocha.it('Success small key', function (done) {
             const hdClient = hdmock.getDefaultClient();
@@ -38,7 +40,7 @@ mocha.describe('PUT', function () {
                 hdmock.getPayloadLength(mocks[0].payload),
                 keyContext, '1',
                 (err, rawKey) => {
-                    assert.ifError(err);
+                    /* Check generated key */
                     assert.strictEqual(typeof rawKey, 'string');
                     const parts = hdclient.keyscheme.deserialize(rawKey);
 
@@ -51,7 +53,14 @@ mocha.describe('PUT', function () {
                     assert.strictEqual(fragment.hostname, endpoint);
                     assert.strictEqual(fragment.port, Number(port));
                     assert.ok(fragment.key, keyContext.objectKey);
-                    done();
+
+                    /* Check cleanup mechanism */
+                    const topic = hdmock.getTopic(hdClient, deleteTopic);
+                    hdmock.strictCompareDeleteTopicContent(
+                        topic, undefined);
+
+                    /* Check for errors */
+                    done(err);
                 });
         });
 
@@ -78,7 +87,7 @@ mocha.describe('PUT', function () {
                 hdmock.getPayloadLength(content),
                 keyContext, '1',
                 (err, rawKey) => {
-                    assert.ifError(err);
+                    /* Check generated key */
                     assert.strictEqual(typeof rawKey, 'string');
                     const parts = hdclient.keyscheme.deserialize(rawKey);
 
@@ -91,7 +100,14 @@ mocha.describe('PUT', function () {
                     assert.strictEqual(fragment.hostname, endpoint);
                     assert.strictEqual(fragment.port, Number(port));
                     assert.ok(fragment.key, keyContext.objectKey);
-                    done();
+
+                    /* Check cleanup mechanism */
+                    const topic = hdmock.getTopic(hdClient, deleteTopic);
+                    hdmock.strictCompareDeleteTopicContent(
+                        topic, undefined);
+
+                    /* Check for errors */
+                    done(err);
                 });
         });
 
@@ -115,6 +131,12 @@ mocha.describe('PUT', function () {
                 hdmock.getPayloadLength(mocks[0].payload),
                 keyContext, '1',
                 err => {
+                    /* Check cleanup mechanism */
+                    const topic = hdmock.getTopic(hdClient, deleteTopic);
+                    hdmock.strictCompareDeleteTopicContent(
+                        topic, undefined);
+
+                    /* Check for errors */
                     assert.strictEqual(err.infos.status, mocks[0].statusCode);
                     done();
                 });
@@ -141,15 +163,326 @@ mocha.describe('PUT', function () {
                 hdmock.streamString(mocks[0].payload),
                 hdmock.getPayloadLength(mocks[0].payload),
                 keyContext, '1',
-                /* PUT is considered successful on timeout as we don't
-                 * whether it really was stored or not
-                 */
                 (err, rawKey) => {
                     assert.ok(!called);
                     called = true;
-                    assert.ok(err);
+
+                    /* Check cleanup mechanism */
+                    const topic = hdmock.getTopic(hdClient, deleteTopic);
+                    const expectedLoggedErrors = [{
+                        rawKey,
+                        toDelete: [[0, 0]],
+                    }];
+                    hdmock.strictCompareDeleteTopicContent(
+                        topic, expectedLoggedErrors);
+
+                    /* Check for errors */
+                    assert.strictEqual(err.infos.status, 500);
                     done();
                 });
+        });
+    });
+
+    mocha.describe('Multiple hyperdrives', function () {
+        mocha.describe('Replication', function () {
+            mocha.it('All Success', function (done) {
+                const hdClient = hdmock.getDefaultClient({
+                    nLocations: 3,
+                    code: 'CP',
+                    nData: 3,
+                    nCoding: 0,
+                });
+                const content = 'Je suis une mite en pullover';
+                const mocks = [
+                    {
+                        statusCode: 200,
+                        payload: content,
+                        contentType: 'data',
+                    },
+                    {
+                        statusCode: 200,
+                        payload: content,
+                        contentType: 'data',
+                    },
+                    {
+                        statusCode: 200,
+                        payload: content,
+                        contentType: 'data',
+                    },
+                ];
+                const keyContext = {
+                    objectKey: 'bestObjEver',
+                };
+
+                hdmock.mockPUT(hdClient.options, keyContext, mocks);
+
+                hdClient.put(
+                    hdmock.streamString(content),
+                    hdmock.getPayloadLength(content),
+                    keyContext, '1',
+                    (err, rawKey) => {
+                        /* Check generated key */
+                        assert.strictEqual(typeof rawKey, 'string');
+                        const parts = hdclient.keyscheme.deserialize(rawKey);
+                        assert.strictEqual(parts.nDataParts, 3);
+                        assert.strictEqual(parts.nCodingParts, 0);
+                        assert.strictEqual(parts.nChunks, 1);
+
+                        /* Check cleanup mechanism */
+                        const topic = hdmock.getTopic(hdClient, deleteTopic);
+                        hdmock.strictCompareDeleteTopicContent(
+                            topic, undefined);
+
+                        /* Check for errors */
+                        done(err);
+                    });
+            });
+
+            mocha.it('Single failure', function (done) {
+                const hdClient = hdmock.getDefaultClient({
+                    nLocations: 3,
+                    code: 'CP',
+                    nData: 3,
+                    nCoding: 0,
+                });
+                const content = fs.createReadStream(
+                    'tests/functional/random_payload');
+                const mocks = [
+                    {
+                        statusCode: 200,
+                        payload: content,
+                        contentType: 'data',
+                    },
+                    {
+                        statusCode: 200,
+                        payload: content,
+                        contentType: 'data',
+                    },
+                    {
+                        statusCode: 500,
+                        payload: content,
+                        contentType: 'data',
+                    },
+                ];
+                const keyContext = {
+                    objectKey: 'bestObjEver',
+                };
+
+                hdmock.mockPUT(hdClient.options, keyContext, mocks);
+
+                hdClient.put(
+                    content,
+                    hdmock.getPayloadLength(content),
+                    keyContext, '1',
+                    (err, rawKey) => {
+                        /* Check for errors */
+                        assert.ok(err);
+                        assert.strictEqual(err.infos.status, 500);
+
+                        /* Key should still be valid */
+                        assert.strictEqual(typeof rawKey, 'string');
+                        const parts = hdclient.keyscheme.deserialize(rawKey);
+                        assert.strictEqual(parts.nDataParts, 3);
+                        assert.strictEqual(parts.nCodingParts, 0);
+                        assert.strictEqual(parts.nChunks, 1);
+
+                        /* Check cleanup mechanism */
+                        const topic = hdmock.getTopic(hdClient, deleteTopic);
+                        const expectedLoggedErrors = [{
+                            rawKey,
+                            toDelete: [[0, 0], [0, 1]],
+                        }];
+                        hdmock.strictCompareDeleteTopicContent(
+                            topic, expectedLoggedErrors);
+                        done();
+                    });
+            });
+
+            mocha.it('Double failure', function (done) {
+                const hdClient = hdmock.getDefaultClient({
+                    nLocations: 3,
+                    code: 'CP',
+                    nData: 3,
+                    nCoding: 0,
+                });
+                const content = fs.createReadStream(
+                    'tests/functional/random_payload');
+                const mocks = [
+                    {
+                        statusCode: 403,
+                        payload: content,
+                        contentType: 'data',
+                    },
+                    {
+                        statusCode: 200,
+                        payload: content,
+                        contentType: 'data',
+                    },
+                    {
+                        statusCode: 500,
+                        payload: content,
+                        contentType: 'data',
+                    },
+                ];
+                const keyContext = {
+                    objectKey: 'bestObjEver',
+                };
+
+                hdmock.mockPUT(hdClient.options, keyContext, mocks);
+
+                hdClient.put(
+                    content,
+                    hdmock.getPayloadLength(content),
+                    keyContext, '1',
+                    (err, rawKey) => {
+                        /* Check for errors */
+                        assert.ok(err);
+                        assert.strictEqual(err.infos.status, 500);
+
+                        /* Key should still be valid */
+                        assert.strictEqual(typeof rawKey, 'string');
+                        const parts = hdclient.keyscheme.deserialize(rawKey);
+                        assert.strictEqual(parts.nDataParts, 3);
+                        assert.strictEqual(parts.nCodingParts, 0);
+                        assert.strictEqual(parts.nChunks, 1);
+
+                        /* Check cleanup mechanism */
+                        const topic = hdmock.getTopic(hdClient, deleteTopic);
+                        const expectedLoggedErrors = [{
+                            rawKey,
+                            toDelete: [[0, 1]],
+                        }];
+                        hdmock.strictCompareDeleteTopicContent(
+                            topic, expectedLoggedErrors);
+                        done();
+                    });
+            });
+
+            mocha.it('Timeout < 50%', function (done) {
+                const hdClient = hdmock.getDefaultClient({
+                    nLocations: 3,
+                    code: 'CP',
+                    nData: 3,
+                    nCoding: 0,
+                });
+                const content = fs.createReadStream(
+                    'tests/functional/random_payload');
+                const mocks = [
+                    {
+                        statusCode: 200,
+                        payload: content,
+                        contentType: 'data',
+                    },
+                    {
+                        statusCode: 200,
+                        payload: content,
+                        contentType: 'data',
+                        timeoutMs: hdClient.options.requestTimeoutMs + 10,
+                    },
+                    {
+                        statusCode: 200,
+                        payload: content,
+                        contentType: 'data',
+                    },
+                ];
+                const keyContext = {
+                    objectKey: 'bestObjEver',
+                };
+
+                hdmock.mockPUT(hdClient.options, keyContext, mocks);
+
+                hdClient.put(
+                    content,
+                    hdmock.getPayloadLength(content),
+                    keyContext, '1',
+                    (err, rawKey) => {
+                        /* Key should still be valid */
+                        assert.strictEqual(typeof rawKey, 'string');
+                        const parts = hdclient.keyscheme.deserialize(rawKey);
+                        assert.strictEqual(parts.nDataParts, 3);
+                        assert.strictEqual(parts.nCodingParts, 0);
+                        assert.strictEqual(parts.nChunks, 1);
+
+                        /* Check cleanup mechanism */
+                        const topic = hdmock.getTopic(hdClient, deleteTopic);
+                        hdmock.strictCompareDeleteTopicContent(
+                            topic, undefined);
+
+                        /* Check for errors */
+                        done(err);
+                    });
+            });
+
+            mocha.it('Timeout >= 50%', function (done) {
+                /**
+                 * Should result in global failure and
+                 * request all fragments to be cleaned up
+                 */
+                const hdClient = hdmock.getDefaultClient({
+                    nLocations: 4,
+                    code: 'CP',
+                    nData: 4,
+                    nCoding: 0,
+                });
+                const content = fs.createReadStream(
+                    'tests/functional/random_payload');
+                const mocks = [
+                    {
+                        statusCode: 200,
+                        payload: content,
+                        contentType: 'data',
+                    },
+                    {
+                        statusCode: 200,
+                        payload: content,
+                        contentType: 'data',
+                    },
+                    {
+                        statusCode: 200,
+                        payload: content,
+                        contentType: 'data',
+                        timeoutMs: hdClient.options.requestTimeoutMs + 10,
+                    },
+                    {
+                        statusCode: 200,
+                        payload: content,
+                        contentType: 'data',
+                        timeoutMs: hdClient.options.requestTimeoutMs + 10,
+                    },
+                ];
+                const keyContext = {
+                    objectKey: 'bestObjEver',
+                };
+
+                hdmock.mockPUT(hdClient.options, keyContext, mocks);
+
+                hdClient.put(
+                    content,
+                    hdmock.getPayloadLength(content),
+                    keyContext, '1',
+                    (err, rawKey) => {
+                        /* Check for errors */
+                        assert.ok(err);
+                        assert.strictEqual(err.infos.status, 500);
+
+                        /* Key should still be valid */
+                        assert.strictEqual(typeof rawKey, 'string');
+                        const parts = hdclient.keyscheme.deserialize(rawKey);
+                        assert.strictEqual(parts.nDataParts, 4);
+                        assert.strictEqual(parts.nCodingParts, 0);
+                        assert.strictEqual(parts.nChunks, 1);
+
+                        /* Check cleanup mechanism */
+                        const topic = hdmock.getTopic(hdClient, deleteTopic);
+                        const expectedLoggedErrors = [{
+                            rawKey,
+                            toDelete: [[0, 0], [0, 1], [0, 2], [0, 3]],
+                        }];
+                        hdmock.strictCompareDeleteTopicContent(
+                            topic, expectedLoggedErrors);
+                        done();
+                    });
+            });
         });
     });
 });
