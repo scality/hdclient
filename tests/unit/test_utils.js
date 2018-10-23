42d6a51eaf06f7e708f75c9dfad61dae0ffdf034
Maxime Lubin <maxime.lubin@scality.com>
2018-10-23 14:46:03 +0200

    RING-28537 Helper to sample from Categorical Distribution
    2 small addition in lib/utils.js to prepare for data placement:
    1/ copyArray - to perform shallow copy
    2/ categoricalSample - sampling from unnormalized categorical distribution

--- tests/unit/test_utils.js
+++ tests/unit/test_utils.js
@@ -79,4 +79,71 @@ mocha.describe('Helper functions', function () {
             done();
         });
     });
+
+    mocha.it('Shallow array copy', function (done) {
+        const original = [1, '2', true, { test: 42 }, [1, 2, 3]];
+        const copy = libUtils.copyArray(original);
+        copy[0] = 3.14159;
+        copy[1] = 'paf!';
+        copy[2] = {};
+        copy[3] = null;
+        copy[4] = [777];
+
+        assert.strictEqual(original[0], 1);
+        assert.strictEqual(original[1], '2');
+        assert.strictEqual(original[2], true);
+        assert.deepStrictEqual(original[3], { test: 42 });
+        assert.deepStrictEqual(original[4], [1, 2, 3]);
+        done();
+    });
+
+    mocha.describe('Categorical distribution sampling', function () {
+        const nTrialsSmall = 100;
+        const nTrialsBig = 10000;
+
+        mocha.it('Collapsed distribution', function (done) {
+            for (let i = 0; i < nTrialsSmall; i++) {
+                const sample = libUtils.categoricalSample([1.0]);
+                assert.strictEqual(0, sample);
+            }
+            for (let i = 0; i < nTrialsSmall; i++) {
+                const sample = libUtils.categoricalSample([0, 0, 1, 0.0]);
+                assert.strictEqual(2, sample);
+            }
+            done();
+        });
+
+        mocha.it('Weird behavior - passing wrong normalization hint', function (done) {
+            const badConstant = 1e-8;
+            const weights = [0.5, 0.5];
+            const counts = new Array(2).fill(0);
+            for (let i = 0; i < nTrialsBig; i++) {
+                const sample = libUtils.categoricalSample(weights, badConstant);
+                counts[sample] += 1;
+            }
+            /* In this case, all samples are skewed onto entry 0 */
+            assert.ok(counts[0] > nTrialsBig * 0.999);
+            done();
+        });
+
+        mocha.it('Random categorical distribution', function (done) {
+            const nCategories = 10;
+            const scale = Math.random() * 10000;
+            const weights = libUtils.range(nCategories).map(() => Math.random() * scale);
+            const sum = weights.reduce((a, b) => a + b, 0.0);
+            const counts = new Array(nCategories).fill(0);
+            for (let i = 0; i < nTrialsBig; i++) {
+                const sample = libUtils.categoricalSample(weights, sum);
+                counts[sample] += 1;
+            }
+
+            /* Check distribution against expected sample ratios */
+            counts.forEach((c, i) => {
+                const expectedRatio = weights[i] / sum;
+                const ratio = c / nTrialsBig;
+                assert.ok(Math.abs(expectedRatio - ratio) < 0.01);
+            });
+            done();
+        });
+    });
 });
