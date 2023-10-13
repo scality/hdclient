f9b36ad30c067dfae55ef39f2f1e61ae01408f6e
Benoit A. <benoit@scality.com>
2023-10-13 16:37:43 +0200

    HD-3340 add a test capturing the response stream object
    * make sure the stream is ended properly

--- tests/unit/hd.ts
+++ tests/unit/hd.ts
@@ -1,10 +1,14 @@
 'use strict'; // eslint-disable-line strict
 
 import * as assert from 'assert';
+import * as sinon from "sinon";
 import * as nock from 'nock';
 import * as stream from 'stream';
+import http = require('http');
 import { HDProxydClient, HDProxydError, HDProxydOptions } from '../../src/hdcontroller';
 import { shuffle } from '../../src/shuffle';
+import {setTimeout} from 'timers/promises';
+import { URL } from 'url';
 
 describe('HD Controller client', () => {
     describe('test init', () => {
@@ -109,6 +113,56 @@ describe('HD Controller client', () => {
                 });
             });
 
+            it('should drain the response pipe in case of error', (done) => {
+                nock('http://1.2.3.4:12345')
+                .post('/store/')
+                .reply(500, function () {
+                    const upStream = new stream.Readable();
+                    upStream.push('upload');
+                    upStream.push(null);
+                    //upStream.on("close", () => {received = true;});
+                    return upStream;
+                  });
+                const upStream = new stream.Readable();
+                upStream.push('upload');
+                upStream.push(null);
+
+                const h = new HDProxydClient({
+                    bootstrap: ['1.2.3.4:12345'],
+                } as HDProxydOptions);
+
+                // Let's replace http.request by a new capturing CB
+                let received: boolean = false;
+                const prev = http.request;
+                sinon.replace(http, "request", (v, cb) => {
+                    return prev(v, (response) => {
+                        response.on("end", () => {received = true;});
+                        return cb(response)
+                })});
+
+                h.put(upStream, 6, {
+                    bucketName: 'test',
+                    owner: 'test',
+                    // tslint:disable-next-line: object-literal-sort-keys
+                    namespace: 'nspace'}, 'nope',
+                    async (err: HDProxydError|undefined , key: string|undefined) => {
+                        // Remove fake http.request
+                        sinon.restore();
+                        if (!err) {
+                            done(Error('error expected, got success'));
+                            return;
+                        }
+
+                        // Let's give it a few scheduler runs
+                        // to make events are consumed
+                        await setTimeout(100);
+                        if (!received) {
+                            done(Error("stream was not consumed"));
+                            return;
+                        }
+                        done();
+                    });
+                });
         });
     describe('failover test', () => {
             it('should raise an error if key is not returned', (done) => {
